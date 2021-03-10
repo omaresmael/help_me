8b93d7826d49ba08e4fe90961a5b6c93de3589ab
emankamal <emankamalnasr33@gmail.com>
2021-03-10 14:46:01 +0200

    cleaning-login-mass

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.master-without-nav')
 @section('title')
-Login
+Login To Help Me
 @endsection
 @section('body')
 <body class="auth-body-bg">
 @endsection
 @section('content')
 <div class="home-btn d-none d-sm-block">
-    <a href="{{url('index')}}"><i class="mdi mdi-home-variant h2 text-white"></i></a>
+    <a href="{{url('index')}}"></a>
 </div>
 <div>
     <div class="container-fluid p-0">
@@ -20,11 +20,11 @@ Login
                                 <div>
                                     <div class="text-center">
                                         <div>
-                                            <a href="{{url('index')}}" class="logo"><img src="{{ URL::asset('/assets/images/logo.png')}}" height="20" alt="logo"></a>
+                                            <a href="{{url('/')}}" class="logo"><img src="{{ URL::asset('/assets/img/logo.png')}}" height="20" alt="logo"></a>
                                         </div>
 
                                         <h4 class="font-size-18 mt-4">Welcome Back !</h4>
-{{--                                        <p class="text-muted">Sign in to continue to Nazox.</p>--}}
+                                        <p class="text-muted">Sign in to continue to Help Me.</p>
                                     </div>
 
                                     <div class="p-2 mt-5">
@@ -32,10 +32,10 @@ Login
                                             @csrf
 
                                             <div class="form-group auth-form-group-custom mb-4">
-                                                <i class="ri-user-2-line auti-custom-input-icon"></i>
-                                                <label for="email">{{ __('E-Mail Address') }}</label>
-                                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" placeholder="Enter Email">
-                                                @error('email')
+                                                
+                                                <label for="username">User Name</label>
+                                                <input id="username" type="username" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="email" placeholder="Enter Your User Name">
+                                                @error('username')
                                                     <span class="invalid-feedback" role="alert">
                                                         <strong>{{ $message }}</strong>
                                                     </span>
@@ -43,7 +43,7 @@ Login
                                             </div>
 
                                             <div class="form-group auth-form-group-custom mb-4">
-                                                <i class="ri-lock-2-line auti-custom-input-icon"></i>
+                                            
                                                 <label for="password">{{ __('Password') }}</label>
                                                 <input type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" id="password" placeholder="Enter password">
                                                 @error('password')
@@ -52,26 +52,12 @@ Login
                                                     </span>
                                                 @enderror
                                             </div>
-
-                                            <div class="custom-control custom-checkbox">
-                                                <input type="checkbox" class="custom-control-input" id="customControlInline">
-                                                <label class="custom-control-label" for="customControlInline">Remember me</label>
-                                            </div>
-
                                             <div class="mt-4 text-center">
                                                 <button class="btn btn-primary w-md waves-effect waves-light" type="submit">{{ __('Log In') }}</button>
                                             </div>
-
-                                            <div class="mt-4 text-center">
-                                                @if (Route::has('password.request'))
-                                                    <a href="{{ route('password.request') }}" class="text-muted"><i class="mdi mdi-lock mr-1"></i> {{ __('Forgot your password?') }}</a>
-                                                @endif
-                                            </div>
                                         </form>
                                     </div>
-
                                     <div class="mt-5 text-center">
-                                        <p>Don't have an account ? <a href="{{url('register')}}" class="font-weight-medium text-primary"> Register </a> </p>
                                         <p><script>document.write(new Date().getFullYear())</script>© Quantum.  
                                     </div>
                                 </div>
@@ -81,7 +67,6 @@ Login
                 </div>
             </div>
             <div class="col-lg-8">
-                <div class="authentication-bg">
                 <div class="bg-overlay"></div>
             </div>
         </div>
