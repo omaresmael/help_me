e5071739e1387cff80a1772577fa8db80518dfeb
omarhossameldin <okandil273@gmail.com>
2021-05-03 18:20:03 +0200

    finish-edit-view

--- resources/views/Teacher/edit.blade.php
+++ resources/views/Teacher/edit.blade.php
@@ -8,14 +8,15 @@
                     <p class="card-category">ادخال البيانات المطلوبة</p>
                 </div>
                 <div class="card-body">
-                    <form action="{{route('teachers.store')}}" method="post">
+                    <form action="{{route('teachers.update', $teacher->id)}}" method="post">
                         @csrf
+                        @method('PATCH')
                         <div class="row">
 
                             <div class="col-md-3">
                                 <div class="form-group">
                                     <label class="bmd-label-floating">اسم المعلم</label>
-                                    <input type="text" name="name" value="{{old('name')}}" class="form-control" required>
+                                    <input type="text" name="name" value="{{$teacher->name}}" class="form-control" required>
                                     @if($errors->has("name"))
                                         <small style="color: red">{{$errors->first('name')}}</small>
                                     @endif
@@ -25,7 +26,7 @@
                             <div class="col-md-3">
                                 <div class="form-group">
                                     <label class="bmd-label-floating">التخصص</label>
-                                    <input type="text" name="speciality" value="{{old('speciality')}}"class="form-control" required>
+                                    <input type="text" name="speciality" value="{{$teacher->speciality}}"class="form-control" required>
                                     @if($errors->has("speciality"))
                                         <small style="color: red">{{$errors->first('speciality')}}</small>
                                     @endif
@@ -34,7 +35,7 @@
                             <div class="col-md-3">
                                 <div class="form-group">
                                     <label class="bmd-label-floating">المؤهل</label>
-                                    <input type="text" name="qualification" value="{{old('qualification')}}" class="form-control" required>
+                                    <input type="text" name="qualification" value="{{$teacher->qualification}}" class="form-control" required>
                                     @if($errors->has("qualification"))
                                         <small style="color: red">{{$errors->first('qualification')}}</small>
                                     @endif
@@ -43,7 +44,7 @@
                             <div class="col-md-3">
                                 <div class="form-group">
                                     <label class="bmd-label-floating">الوظيفة</label>
-                                    <input type="text" name="job" value="{{old('job')}}" class="form-control" required>
+                                    <input type="text" name="job" value="{{$teacher->job}}" class="form-control" required>
                                     @if($errors->has("job"))
                                         <small style="color: red">{{$errors->first('job')}}</small>
                                     @endif
@@ -52,7 +53,7 @@
                             <div class="col-md-3">
                                 <div class="form-group">
                                     <label class="bmd-label-floating">الرقم المدني</label>
-                                    <input type="text" name="national_number" value="{{old('national_number')}}" class="form-control" required>
+                                    <input type="text" name="national_number" value="{{$teacher->national_number}}" class="form-control" required>
                                     @if($errors->has("national_number"))
                                         <small style="color: red">{{$errors->first('national_number')}}</small>
                                     @endif
@@ -61,7 +62,7 @@
                             <div class="col-md-3">
                                 <div class="form-group">
                                     <label class="bmd-label-floating">الجنسية</label>
-                                    <input type="text" name="nationality" value="{{old('nationality')}}" class="form-control" required>
+                                    <input type="text" name="nationality" value="{{$teacher->nationality}}" class="form-control" required>
                                     @if($errors->has("nationality"))
                                         <small style="color: red">{{$errors->first('nationality')}}</small>
                                     @endif
@@ -70,7 +71,7 @@
                             <div class="col-md-3">
                                 <div class="form-group">
                                         <label class="bmd-label-floating">تاريخ الميلاد</label>
-                                        <input type="text" name="birth_day" value="{{old('birth_day')}}" class="form-control datepicker" required>
+                                        <input type="text" name="birth_day" value="{{$teacher->birth_day}}" class="form-control datepicker" required>
                                         @if($errors->has("birth_day"))
                                             <small style="color: red">{{$errors->first('birth_day')}}</small>
                                         @endif
@@ -79,7 +80,7 @@
                             <div class="col-md-3">
                                 <div class="form-group">
                                     <label class="bmd-label-floating">رقم موافقة الهيئة</label>
-                                    <input type="text" name="entity_acceptance_number" value="{{old('entity_acceptance_number')}}" class="form-control " required>
+                                    <input type="text" name="entity_acceptance_number" value="{{$teacher->entity_acceptance_number}}" class="form-control " required>
                                     @if($errors->has("entity_acceptance_number"))
                                         <small style="color: red">{{$errors->first('entity_acceptance_number')}}</small>
                                     @endif
@@ -88,7 +89,7 @@
                             <div class="col-md-3">
                                 <div class="form-group" >
                                     <label class="bmd-label-floating">تاريخ موافقة الهيئة</label>
-                                    <input type="text" name="entity_acceptance_date" value="{{old('entity_acceptance_date')}}" class="form-control datepicker" required>
+                                    <input type="text" name="entity_acceptance_date" value="{{$teacher->entity_acceptance_date}}" class="form-control datepicker" required>
                                     @if($errors->has("entity_acceptance_date"))
                                         <small style="color: red">{{$errors->first('entity_acceptance_date')}}</small>
                                     @endif
@@ -116,7 +117,7 @@
                             </div>
 
                         </div>
-                        <button type="submit" class="btn btn-primary pull-right" style="float: left;">اضافة معلم</button>
+                        <button type="submit" class="btn btn-primary pull-right" style="float: left;">تحديث المعلم</button>
                         <div class="clearfix"></div>
                     </form>
                 </div>
